6e6cd34b4bd8607abcc54b743b44912d3bd7ea5a
Mark Saunders <enfortis.tech@gmail.com>
2020-10-23 17:30:15 -0500

    CODE: added SIGTERM

--- web-server-express.js
+++ web-server-express.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 
+process.env.NODE_ENV = "development";
+
 app.get('/', (req, res) => {
-    res.send('Hi from your brand new webserver!');
+    const date = JSON.stringify(Date.now());
+    res.send(`Hi from your brand new webserver the date is: ${date}`);
 });
 const port = 3000;
 
 
-app.listen(port, () => console.log(`server runing on port: ${port}`));
+const server = app.listen(port, () => console.log(`server runing on port: ${port}, process.pid ${process.pid}, process.env = ${process.env.NODE_ENV}`));
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('Process terminated');
+    })
+})
